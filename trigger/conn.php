95aecefb4bfb60dc14f335fec0d8fb1786811880
Allan <allan.cabug-os@splitsecondsoftware.com>
2022-08-30 21:04:46 +0800

    first commit

--- trigger/conn.php
+++ trigger/conn.php
@@ -1,13 +1,13 @@
 <?php
 	//$servername = "192.168.254.100";
-// $servername = "91.192.194.139";
-// $username = "idessrte_sample";
-// $password = 'Jc-rt%^hK&B$_sample';
-// $database = "idessrte_sample";
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "auxiliary";
+$servername = "91.192.194.139";
+$username = "idessrte_sample";
+$password = 'Jc-rt%^hK&B$_sample';
+$database = "idessrte_sample";
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $database = "auxiliary";
 
 $conn = mysqli_connect($servername,$username,$password,$database);
 if (!$conn) {
